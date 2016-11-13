a2f7961c3456f184e428be3b38ca339a2aba2a8d
Sam Presnal <boiler9000@gmail.com>
2016-11-13 13:13:54 -0600

    Change import syntax
    Potential travis fix

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import league from '../imports/components/league/league';
+import { angular } from 'angular';
+import { angularMeteor } from 'angular-meteor';
+import { league } from '../imports/components/league/league';
  
 angular.module('meteor-angular-lol', [
   angularMeteor,
